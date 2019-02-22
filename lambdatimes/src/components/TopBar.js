54b851237592cf1992a47d2ec7e1f3a49a69100d
clem9281 <clem9281@gmail.com>
2019-02-22 10:35:06 -0700

    refactored styled components

--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -28,34 +28,69 @@ const TopBarContainer = styled.div`
     width: 1280px;
   }
 `;
-// const TopBarContainer = styled.div`
+const TopBarContainerLeft = styled.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+`;
+const TopBarContainerLeftSpan = styled.span`
+  cursor: pointer;
+  margin-right: 25%;
+  font-weight: bold;
+`;
+const TopBarContainerCenter = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  flex: 3;
+  font-size: 9px;
+`;
+const TopBarContainerCenterSpan = styled.span`
+  cursor: pointer;
+  margin-right: 5%;
+  &:last-child {
+    margin-right: 0;
+  }
+  &:hover {
+    text-decoration: underline;
+  }
+`;
 
-// `;
-// const TopBarContainer = styled.div`
-
-// `;
-// const TopBarContainer = styled.div`
-
-// `;
+const TopBarContainerRight = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+  font-weight: bold;
+`;
+const TopBarContainerRightSpan = styled.span`
+  cursor: pointer;
+`;
 
 const TopBar = () => {
   return (
     <TopBarParent>
       <TopBarContainer>
-        <div className="container-left">
-          <span>TOPICS</span>
-          <span>SEARCH</span>
-        </div>
-        <div className="container-center">
-          <span>GENERAL</span>
-          <span>BROWNBAG</span>
-          <span>RANDOM</span>
-          <span>MUSIC</span>
-          <span>ANNOUNCEMENTS</span>
-        </div>
-        <div className="container-right">
-          <span>LOG IN</span>
-        </div>
+        <TopBarContainerLeft>
+          <TopBarContainerLeftSpan>TOPICS</TopBarContainerLeftSpan>
+          <TopBarContainerLeftSpan>SEARCH</TopBarContainerLeftSpan>
+        </TopBarContainerLeft>
+        <TopBarContainerCenter>
+          <TopBarContainerCenterSpan>GENERAL</TopBarContainerCenterSpan>
+          <TopBarContainerCenterSpan>BROWNBAG</TopBarContainerCenterSpan>
+          <TopBarContainerCenterSpan>RANDOM</TopBarContainerCenterSpan>
+          <TopBarContainerCenterSpan>MUSIC</TopBarContainerCenterSpan>
+          <TopBarContainerCenterSpan>ANNOUNCEMENTS</TopBarContainerCenterSpan>
+        </TopBarContainerCenter>
+        <TopBarContainerRight>
+          <TopBarContainerRightSpan>LOG IN</TopBarContainerRightSpan>
+        </TopBarContainerRight>
       </TopBarContainer>
     </TopBarParent>
   );
